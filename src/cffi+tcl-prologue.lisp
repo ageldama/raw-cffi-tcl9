3ab73105ddd7202c5a2cbf914193002ff5252844
jonghyouk yun <ageldama@gmail.com>
2026-05-20 16:15:12 +0900

    garrgh

--- src/cffi+tcl-prologue.lisp
+++ src/cffi+tcl-prologue.lisp
@@ -1,28 +1,27 @@
 (in-package #:raw-cffi-tcl9)
 
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
 
-(define-foreign-library libtcl
-  (:unix    (:or #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl9.0.so")))
-  (:darwin  (:default #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl9.0.dylib")))
-  (:windows (:or #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl90"
-                       "tcl90" "tcl90t")))
-  (t (:default #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl"))))
+  (define-foreign-library libtcl
+      (:unix    (:or #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl9.0.so")))
+    (:darwin  (:default #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl9.0.dylib")))
+    (:windows (:or #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl90"
+                         "tcl90" "tcl90t")))
+    (t (:default #.(or (uiop:getenv "CFFI_LIBTCL") "libtcl"))))
 
-(use-foreign-library libtcl)
+  (use-foreign-library libtcl)
 
+  (defctype tcl-uni-char :uint)
+  (defconstant +tcl-utf-max+ 4)
 
-(defctype tcl-uni-char :uint)
-(defconstant +tcl-utf-max+ 4)
+  (if (eq (cffi:foreign-type-size :long-long)
+          (cffi:foreign-type-size :long))
+      (progn
+        (defctype tcl-wide-int :long)
+        (defctype tcl-wide-uint :unsigned-long))
+      (progn
+        (defctype tcl-wide-int :long-long)
+        (defctype tcl-wide-uint :unsigned-long-long)))
 
-
-
-(if (eq (cffi:foreign-type-size :long-long)
-        (cffi:foreign-type-size :long))
-    (progn
-      (defctype tcl-wide-int :long)
-      (defctype tcl-wide-uint :unsigned-long))
-    (progn
-      (defctype tcl-wide-int :long-long)
-      (defctype tcl-wide-uint :unsigned-long-long)))
-
+  )
